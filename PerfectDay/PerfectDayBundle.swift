19e90faa28f0ec7e58276af9161d2b64aed4b205
Scott Lydon <scottlydon18@gmail.com>
2023-01-14 01:55:19 -0800

    First

---
+++ PerfectDay/PerfectDayBundle.swift
@@ -0,0 +1,17 @@
+//
+//  PerfectDayBundle.swift
+//  PerfectDay
+//
+//  Created by Scott Lydon on 1/14/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct PerfectDayBundle: WidgetBundle {
+    var body: some Widget {
+        PerfectDay()
+        PerfectDayLiveActivity()
+    }
+}
---
+++ PerfectDayWidget/PerfectDayWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerfectDayWidgetApp.swift
+//  PerfectDayWidget
+//
+//  Created by Scott Lydon on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct PerfectDayWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
